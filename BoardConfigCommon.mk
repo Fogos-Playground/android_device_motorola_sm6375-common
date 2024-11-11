314d635ffa2dfeb43304f1b4c826c93b12c63843
Christopher Ferris <cferris@google.com>
2024-11-12 00:52:47 +0800

    sm6375-common: Switch to IGNORE_PREFER32_ON_DEVICE
    In aosp, the name of the variable changed from
    FORCE_MULTILIB_FIRST_ON_DEVICE to IGNORE_PREFER32_ON_DEVICE.
    
    So actually modify this so the new devices don't have 32 bit
    servers.
    
    Test: Verified that drmserver/mediaserver are 64 bit processes.
    Change-Id: I282f5129bcc91d99e924f514b01fc4e7e37e9ea9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,7 +21,7 @@ TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := kryo385
 
 # Force any prefer32 targets to be compiled as 64 bit.
-FORCE_MULTILIB_FIRST_ON_DEVICE := true
+IGNORE_PREFER32_ON_DEVICE := true
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
