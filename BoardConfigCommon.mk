0d907281aea64fff91b2d0fde64dd19ff6bf1292
Christopher Ferris <cferris@google.com>
2025-03-29 11:39:08 +0800

    sm6375-common: Enable 32 bit building, but only 64 bit zygote.
    Bug: 229787331
    Test: Build/Boot in progress.
    Change-Id: I4686b567fd96a6cd7c3f6e269f0424811c0b3d6a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,6 +29,9 @@ TARGET_DYNAMIC_64_32_MEDIASERVER := true
 # Include 64-bit drmserver to support 64-bit only devices
 TARGET_DYNAMIC_64_32_DRMSERVER := true
 
+# Enable 64-bit for non-zygote.
+ZYGOTE_FORCE_64 := true
+
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
